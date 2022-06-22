96b3028ffe21a4ea8c68b8c5ab966e21f4ccf320
aggink <73179222+aggink@users.noreply.github.com>
2022-06-22 22:29:14 +0300

    Реализован контроллер для Game

--- InfoGames/Controllers/GameController.cs
+++ InfoGames/Controllers/GameController.cs
@@ -1,14 +1,138 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InfoGames.WebAPI.Infrastructure.Managers.Interfaces;
+using InfoGames.WebAPI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InfoGames.WebAPI.Controllers;
 
 [ApiController]
-[Route("games")]
+[Route("api/games")]
 [Produces("application/json")]
 public class GameController : Controller
 {
-    public GameController()
-    {
+    private readonly IGameManager _manager;
+    private readonly ILogger<GameController> _logger;
 
+    public GameController(IGameManager manager, ILogger<GameController> logger)
+    {
+        _manager = manager;
+        _logger = logger;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<JsonResult> GetById(Guid id)
+    {
+        if (!ModelState.IsValid || id == Guid.Empty)
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status400BadRequest
+            };
+
+        var result = await _manager.GetById(id);
+        if (result == null)
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status404NotFound
+            };
+
+        return Json(result);
+    }
+
+    [HttpPost]
+    public async Task<JsonResult> Create(CreateGameViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status400BadRequest
+            };
+
+        var result = await _manager.CreateAsync(model);
+        if (!result)
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+        return new JsonResult(null)
+        {
+            StatusCode = StatusCodes.Status200OK
+        };
+    }
+
+    [HttpPut]
+    public async Task<JsonResult> Update(UpdateGameViewModel model)
+    {
+        if (!ModelState.IsValid || model.Id == Guid.Empty)
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status400BadRequest
+            };
+
+        var result = await _manager.UpdateAsync(model);
+        if (!result)
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+        return new JsonResult(null)
+        {
+            StatusCode = StatusCodes.Status200OK
+        };
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<JsonResult> Delete(Guid id)
+    {
+        if (!ModelState.IsValid || id == Guid.Empty)
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status400BadRequest
+            };
+
+        var result = await _manager.DeleteAsync(id);
+        if (!result)
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+        return new JsonResult(null)
+        {
+            StatusCode = StatusCodes.Status200OK
+        };
+    }
+
+    [HttpGet]
+    public JsonResult GetAll()
+    {
+        var result = _manager.GetAll();
+        if(result == null || result.Count() == 0)
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status404NotFound
+            };
+
+        return Json(result);
+    }
+
+    [HttpGet]
+    [Route("GetGamesOfGenre/{genre}")]
+    public JsonResult Get(string genre)
+    {
+        if (string.IsNullOrEmpty(genre))
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status400BadRequest
+            };
+
+        var result = _manager.GetGamesOfGenre(genre);
+        if (result == null || result.Count() == 0)
+            return new JsonResult(null)
+            {
+                StatusCode = StatusCodes.Status404NotFound
+            };
+
+        return Json(result);
     }
 }
