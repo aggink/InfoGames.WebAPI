0b0eae12bf7cc9ffdb521d60ae974de6e330d374
aggink <73179222+aggink@users.noreply.github.com>
2022-06-22 19:29:08 +0300

    Реализован менеджер для Game

--- InfoGames/Infrastructure/Managers/GameManager.cs
+++ InfoGames/Infrastructure/Managers/GameManager.cs
@@ -1,13 +1,101 @@
 ﻿using InfoGames.WebAPI.Infrastructure.Managers.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using InfoGames.WebAPI.Infrastructure.Providers.Interfaces;
+using InfoGames.WebAPI.Infrastructure.Repositories.Base;
+using InfoGames.WebAPI.Models;
+using InfoGames.WebAPI.ViewModels;
 
 namespace InfoGames.WebAPI.Infrastructure.Managers;
 
 public class GameManager : IGameManager
 {
+    private readonly IRepository<GameModel> _repository;
+    private readonly IGameProvider _provider;
+    private readonly ILogger<GameManager> _logger;
 
+    public GameManager(IRepository<GameModel> repository, IGameProvider provider, ILogger<GameManager> logger)
+    {
+        _repository = repository;
+        _provider = provider;
+        _logger = logger;
+    }
+
+    public async Task<bool> CreateAsync(CreateGameViewModel model)
+    {
+        var entity = new GameModel()
+        {
+            Id = Guid.NewGuid(),
+            Name = model.Name,
+            StudioDeveloper = model.StudioDeveloper,
+            Genres = string.Join(",", model.Genres)
+        };
+
+        var result = await _repository.CreateAsync(entity);
+        if (result == null) return false;
+
+        return true;
+    }
+
+    public async Task<bool> UpdateAsync(UpdateGameViewModel model)
+    {
+        var entity = new GameModel()
+        {
+            Id = model.Id,
+            Name = model.Name,
+            StudioDeveloper = model.StudioDeveloper,
+            Genres = string.Join(",", model.Genres)
+        };
+
+        var result = await _repository.UpdateAsync(entity);
+        if (result == null) return false;
+
+        return true;
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var result = await _repository.DeleteAsync(id);
+        if (!result) return false;
+
+        return true;
+    }
+
+    public async Task<GameViewModel?> GetById(Guid id)
+    {
+        var result = await _repository.FindByIdAsync(id);
+        if (result == null) return null;
+
+        return new GameViewModel()
+        {
+            Id = result.Id,
+            Name = result.Name,
+            StudioDeveloper = result.StudioDeveloper,
+            Genres = result.Genres.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList()
+        };
+    }
+
+    public IQueryable<GameViewModel> GetAll()
+    {
+        var result = _provider.GetAll();
+
+        return result.Select(x => new GameViewModel()
+        {
+            Id = x.Id,
+            Name = x.Name,
+            StudioDeveloper = x.StudioDeveloper,
+            Genres = x.Genres.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList()
+        });
+    }
+
+    public IQueryable<GameViewModel> GetGamesOfGenre(string genre)
+    {
+        var result = _provider.GetGamesOfGenre(genre);
+
+        return result.Select(x => new GameViewModel()
+        {
+            Id = x.Id,
+            Name = x.Name,
+            StudioDeveloper = x.StudioDeveloper,
+            Genres = x.Genres.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList()
+        });
+    }
 }
--- InfoGames/Infrastructure/Managers/Interfaces/IGameManager.cs
+++ InfoGames/Infrastructure/Managers/Interfaces/IGameManager.cs
@@ -1,6 +1,47 @@
-﻿namespace InfoGames.WebAPI.Infrastructure.Managers.Interfaces;
+﻿using InfoGames.WebAPI.ViewModels;
+
+namespace InfoGames.WebAPI.Infrastructure.Managers.Interfaces;
 
 public interface IGameManager
 {
+    /// <summary>
+    /// Создать сущность в БД
+    /// </summary>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    Task<bool> CreateAsync(CreateGameViewModel model);
 
+    /// <summary>
+    /// Обновить сущность в БД
+    /// </summary>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    Task<bool> UpdateAsync(UpdateGameViewModel model);
+
+    /// <summary>
+    /// Удалить сущность в БД
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<bool> DeleteAsync(Guid id);
+
+    /// <summary>
+    /// Получить сущность по id из БД
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<GameViewModel?> GetById(Guid id);
+
+    /// <summary>
+    /// Получить список всех игр
+    /// </summary>
+    /// <returns></returns>
+    IQueryable<GameViewModel> GetAll();
+
+    /// <summary>
+    /// Получить список игр определенного жанра
+    /// </summary>
+    /// <param name="genre"></param>
+    /// <returns></returns>
+    IQueryable<GameViewModel> GetGamesOfGenre(string genre);
 }
