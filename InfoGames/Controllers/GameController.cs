0453630a46361b430e8fc2517e3abdf762b0e991
aggink <73179222+aggink@users.noreply.github.com>
2022-06-23 19:57:07 +0300

    Убран ILogger и проверка моделей, Json с указанием статуса заменен на BadRequest и NotFound.

--- InfoGames/Controllers/GameController.cs
+++ InfoGames/Controllers/GameController.cs
@@ -10,128 +10,63 @@ namespace InfoGames.WebAPI.Controllers;
 public class GameController : Controller
 {
     private readonly IGameManager _manager;
-    private readonly ILogger<GameController> _logger;
 
-    public GameController(IGameManager manager, ILogger<GameController> logger)
+    public GameController(IGameManager manager)
     {
         _manager = manager;
-        _logger = logger;
     }
 
     [HttpGet("{id}")]
-    public async Task<JsonResult> GetById(Guid id)
+    public async Task<IActionResult> GetById(Guid id)
     {
-        if (!ModelState.IsValid || id == Guid.Empty)
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status400BadRequest
-            };
-
         var result = await _manager.GetById(id);
-        if (result == null)
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status404NotFound
-            };
+        if (result == null) return NotFound();
 
         return Json(result);
     }
 
     [HttpPost]
-    public async Task<JsonResult> Create(CreateGameViewModel model)
+    public async Task<IActionResult> Create(CreateGameViewModel model)
     {
-        if (!ModelState.IsValid)
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status400BadRequest
-            };
-
         var result = await _manager.CreateAsync(model);
-        if (!result)
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status500InternalServerError
-            };
+        if (!result) return BadRequest();
 
-        return new JsonResult(null)
-        {
-            StatusCode = StatusCodes.Status200OK
-        };
+        return Ok();
     }
 
     [HttpPut]
-    public async Task<JsonResult> Update(UpdateGameViewModel model)
+    public async Task<IActionResult> Update(UpdateGameViewModel model)
     {
-        if (!ModelState.IsValid || model.Id == Guid.Empty)
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status400BadRequest
-            };
-
         var result = await _manager.UpdateAsync(model);
-        if (!result)
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status500InternalServerError
-            };
+        if (!result) return BadRequest();
 
-        return new JsonResult(null)
-        {
-            StatusCode = StatusCodes.Status200OK
-        };
+        return Ok();
     }
 
     [HttpDelete("{id}")]
-    public async Task<JsonResult> Delete(Guid id)
+    public async Task<IActionResult> Delete(Guid id)
     {
-        if (!ModelState.IsValid || id == Guid.Empty)
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status400BadRequest
-            };
-
         var result = await _manager.DeleteAsync(id);
-        if (!result)
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status500InternalServerError
-            };
+        if (!result) return BadRequest();
 
-        return new JsonResult(null)
-        {
-            StatusCode = StatusCodes.Status200OK
-        };
+        return Ok();
     }
 
     [HttpGet]
-    public JsonResult GetAll()
+    public IActionResult GetAll()
     {
         var result = _manager.GetAll();
-        if(result == null || result.Count() == 0)
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status404NotFound
-            };
+        if (result == null || result.Count() == 0) return NotFound();
 
         return Json(result);
     }
 
     [HttpGet]
     [Route("GetGamesOfGenre/{genre}")]
-    public JsonResult Get(string genre)
+    public IActionResult Get(string genre)
     {
-        if (string.IsNullOrEmpty(genre))
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status400BadRequest
-            };
-
         var result = _manager.GetGamesOfGenre(genre);
-        if (result == null || result.Count() == 0)
-            return new JsonResult(null)
-            {
-                StatusCode = StatusCodes.Status404NotFound
-            };
+        if (result == null || result.Count() == 0) return NotFound();
 
         return Json(result);
     }
